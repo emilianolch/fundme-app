ced8a76bb084a2a61706fdef8963ecc622b058b5
Emiliano López <emilianolch@gmail.com>
2022-10-23 20:54:31 -0300

    harcoded fund

---
+++ README.md
@@ -0,0 +1,3 @@
+# Fund Me App
+
+Simple front end for the [FundMe](https://github.com/emilianolch/hh-fundme) smart contract.
---
+++ index.js
@@ -0,0 +1,40 @@
+import { ethers } from "./ethers-5.1.esm.min.js";
+import { abi, contractAddress } from "./constants.js";
+
+const connectBtn = document.getElementById("connect");
+const fundBtn = document.getElementById("fund");
+const status = document.getElementById("status");
+
+connectBtn.addEventListener("click", connect);
+fundBtn.addEventListener("click", fund);
+
+async function connect() {
+  if (window.ethereum) {
+    await window.ethereum.request({ method: "eth_requestAccounts" });
+    status.innerHTML = "Connected";
+  } else {
+    status.innerHTML = "Please install Metamask";
+  }
+}
+
+async function fund() {
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(contractAddress, abi, signer);
+  const sendValue = ethers.utils.parseEther("10");
+
+  contract
+    .fund({ value: sendValue })
+    .then((transaction) => {
+      log("Transaction sent. Waiting for confirmation...");
+      return transaction.wait(1);
+    })
+    .then((transactionReceipt) => {
+      log("Confirmed!");
+    })
+    .catch((error) => log(error.message));
+}
+
+function log(message) {
+  status.innerHTML = message;
+}
